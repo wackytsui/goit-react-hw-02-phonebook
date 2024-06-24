8e68d56e3a75e3a1e61c0e340bc606305125fbe9
wackytsui <allan.altavano@gmail.com>
2024-06-24 22:05:26 +0800

    deleted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,62 @@
-import { Component } from 'react';
-import { ContactForm } from './ContactForm/ContactForm';
-import { Filter } from './Filter/Filter';
-import { ContactList } from './ContactList/ContactList';
+// import { Component } from 'react';
+// import { ContactForm } from './ContactForm/ContactForm';
+// import { Filter } from './Filter/Filter';
+// import { ContactList } from './ContactList/ContactList';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+// export class App extends Component {
+//   state = {
+//     contacts: [
+//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//   };
 
-  addContact = newContact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
-  };
+//   addContact = newContact => {
+//     this.setState(prevState => ({
+//       contacts: [...prevState.contacts, newContact],
+//     }));
+//   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
+//   deleteContact = id => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(contact => contact.id !== id),
+//     }));
+//   };
 
-  setFilter = filterValue => {
-    this.setState({
-      filter: filterValue,
-    });
-  };
+//   setFilter = filterValue => {
+//     this.setState({
+//       filter: filterValue,
+//     });
+//   };
 
-  filterContact = () => {
-    const { contacts, filter } = this.state;
-    const filterLowerCase = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filterLowerCase)
-    );
-  };
+//   filterContact = () => {
+//     const { contacts, filter } = this.state;
+//     const filterLowerCase = filter.toLowerCase();
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(filterLowerCase)
+//     );
+//   };
 
-  render() {
-    const { contacts, filter } = this.state;
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm addContact={this.addContact} contacts={contacts} />
+//   render() {
+//     const { contacts, filter } = this.state;
+//     return (
+//       <div>
+//         <h1>Phonebook</h1>
+//         <ContactForm addContact={this.addContact} contacts={contacts} />
 
-        <h2>Contacts</h2>
-        <Filter filter={filter} setFilter={this.setFilter} />
-        <ContactList
-          filterContact={this.filterContact}
-          deleteContact={this.deleteContact}
-        />
-      </div>
-    );
-  }
-}
+//         <h2>Contacts</h2>
+//         <Filter filter={filter} setFilter={this.setFilter} />
+//         <ContactList
+//           filterContact={this.filterContact}
+//           deleteContact={this.deleteContact}
+//         />
+//       </div>
+//     );
+//   }
+// }
 
 
 // export const App = () => {
