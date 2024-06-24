99b7664ca2adb28a83e31936a5e00a381b603672
wackytsui <allan.altavano@gmail.com>
2024-06-24 22:10:54 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -59,19 +59,19 @@
 // }
 
 
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template !!!
-//     </div>
-//   );
-// };
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template !!!
+    </div>
+  );
+};
